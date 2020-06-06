4e9cd3a49eb6215365c12bbae9116b3ba4edebe1
Szoul <53086782+Szoul@users.noreply.github.com>
2020-06-06 15:25:08 +0200

    completed list sorting

--- Intro_to_Lists.py
+++ Intro_to_Lists.py
@@ -32,78 +32,78 @@ if ask_for_listing == 'Yes':
     elif ask_for_randomize_or_change == 'change':
         print ("if you would like to change the order, enter the name of the first person and press enter, then do so again with the person you want to change him/her with")
         print ("you can also change them by entering the respective number of the first member you want to change and press enter and do so with the other member again")
+        print ("____________________________________")
         print ("If you changed your mind or would like to stop, enter 'Stop'")
-
+        print ("If you would like to see the current list, type in 'list'")
 
         while True:
             print ("first name or number:")
             first_name_change = input()
             if first_name_change == 'Stop':
                 break
+            elif first_name_change == "list":
+                show_list()
+                continue
             print ("name or number that you want to change with:")
             second_name_change = input()
             print ("")
+            if second_name_change == 'Stop':
+                break
+            elif second_name_change == "list":
+                show_list()
+                continue
 
             first_int = False
             second_int = False
             try:
-                first_name_change = int(first)
+                first_name_change = int(first_name_change)
+                first_name_change -= 1
                 first_int = True
-                second_name_change = int(second)
+                second_name_change = int(second_name_change)
+                second_name_change -= 1
                 second_int = True
             except:
                 try:
-                    second_name_change = int(second)
+                    second_name_change = int(second_name_change)
+                    second_name_change -= 1
                     second_int = True
                 except:
                     pass
 
             if first_int == False and second_int == False:
-                if first in my_family and second in my_family:
-                    second_position = my_family.index (second)
-                    first_position = my_family.index(first) 
+                if first_name_change in my_family and second_name_change in my_family:
+                    second_position = my_family.index (second_name_change)
+                    first_position = my_family.index(first_name_change) 
 
-                    my_family[first_position] = second
-                    my_family[second_position] = first
+                    my_family[first_position] = second_name_change
+                    my_family[second_position] = first_name_change
                 else:
                     print ("The names you entered aren't in the List")
             elif first_int == True and second_int == False:
-                if first <= len(my_family) and second in my_family:
-                    my_family[my_family.index(second)] = my_family[first]
-                    my_family[first] = second
+                if first_name_change < len(my_family) and second_name_change in my_family:
+                    my_family[my_family.index(second_name_change)] = my_family[first_name_change]
+                    my_family[first_name_change] = second_name_change
                 else:
                     print ("the number of the list item or the word is not part of the list")
             elif second_int == True and first_int == False:
-                if first in my_family and second <= len(my_family):
-                    my_family[my_family.index(first)] = my_family[second]
-                    my_family[second] = first
+                if first_name_change in my_family and second_name_change < len(my_family):
+                    my_family[my_family.index(first_name_change)] = my_family[second_name_change]
+                    my_family[second_name_change] = first_name_change
                 else:
                     print ("the number of the list item or the word is not part of the list")
-            else:
-                if first <= len(my_family) and second <= len(my_family):
-                    first_position = my_family[first]
-                    second_position = my_family [second]
+            elif first_int == True and second_int == True:
+                if first_name_change < len(my_family) and second_name_change < len(my_family):
+                    first_position = my_family[first_name_change]
+                    second_position = my_family [second_name_change]
 
-                    my_family [first] = second_position
-                    my_family[second] = first_position
+                    my_family [first_name_change] = second_position
+                    my_family[second_name_change] = first_position
                 else:
                     print ("At least one of the Numbers you entered does not represent an item in the list")
 
 
-
-
-            if second_name_change == 'Stop':
-                break
-            if first_name_change in my_family and second_name_change in my_family:
-                first_name_change_position = my_family.index(first_name_change)
-                second_name_change_position = my_family.index(second_name_change)
-
-                my_family[first_name_change_position] = second_name_change
-                my_family[second_name_change_position] = first_name_change 
-            else:
-                print ("Your Input did not match what is in the List, please only Enter contents of the List to change it, Enter 'Stop' to exit this function")
-                print ("")
-        
-
-                
-# REPLACE COPIED CODE NAMES
+print ("")
+print ("Here is your finished List:")
+print ("______________________________________")
+print ("")
+show_list()
